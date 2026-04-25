8a3b705b4a3e77ee474635498e1d189d85bf5fa9
Janani-123456 <rjanani29@gmail.com>
2026-04-25 22:07:20 +0530

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package org.testleaf.week1.day1;
+
+public class Chrome {
+public static void main(String[] args) {
+
+    float version = 91.0f ;
+    System.out.println("The float version is " +version);
+    String developer = "Google";
+     System.out.println("The string developer is " +developer);
+    boolean isBeta = false;
+     System.out.println("The boolean isBeta is " +isBeta);
+    int releaseYear = 2008;
+    System.out.println("The int releaseYear is " +releaseYear);    
+    char shortcutKey = 'C';
+    System.out.println("The char shortcutKey is " +shortcutKey);
+
+
+}
+}
---
+++ day1/HelloTestleaf.java
@@ -0,0 +1,9 @@
+package org.testleaf.week1.day1;
+
+public class HelloTestleaf {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Testleaf");
+    }
+}
+
