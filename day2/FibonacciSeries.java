9cc42a1c6742f6b05ea0e0ef177b07eff4c7a153
Janani-123456 <rjanani29@gmail.com>
2026-04-26 15:59:44 +0530

    Add files via upload
    day2

---
+++ day2/FibonacciSeries.java
@@ -0,0 +1,27 @@
+package org.testleaf.week1.day2;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// starting number 0
+		// Ending range 8 --->0, 1, 1, 2, 3, 5, 8, 13
+		// Sum of two preceding one
+
+		int a = 0;
+		int b = 1;
+
+		for (int i = 0; i <= 8; i++) {
+			if (i == 8) {
+				System.out.println("Reached the range");
+				break;
+			}
+			System.out.println(a + " ");
+			int c = a + b;
+			a = b;
+			b = c;
+
+		}
+
+	}
+
+}
---
+++ day2/IsPrimeNumber.java
@@ -0,0 +1,25 @@
+package org.testleaf.week1.day2;
+
+public class IsPrimeNumber {
+    public static void main(String[] args) {
+        int n = 17;
+        boolean isprime = true;
+
+        if (n <= 1) {
+            isprime = false;
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    isprime = false;
+
+                }
+
+            }
+        }
+        if (isprime) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
+        }
+    }
+}
